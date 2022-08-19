d75996c0088e574599e772aafed83055e5beea1d
Sangpil Jo <2019253080@yonsei.ac.kr>
2022-08-19 18:22:26 +0900

    INFO 퍼블리싱 코드 수정
    깃허브 페이지 빌드오류수정

--- resources/JS/stepperbar.js
+++ resources/JS/stepperbar.js
@@ -1,4 +1,4 @@
-//const steps = document.querySelectorAll(".stepper-item");
+// const steps = document.querySelectorAll(".stepper-item");
 // const step = document.querySelector(".stepper-item")
 // const steps = Array.from(document.querySelectorAll(".stepper-item"));
 // const statusbox = document.querySelector(".status");
@@ -14,11 +14,11 @@
 // function stepUpdate() {
 //     const currentStep = steps[stepCounter - 1];
 
-//     if()
-//     currentStep.classList.add("completed");
+//     if ()
+//         currentStep.classList.add("completed");
 // }
 
-//console.log(step.classList.contains('active'));
+// console.log(step.classList.contains('active'));
 
 // console.log(steps[0]);
 // console.log(steps[2].classList.contains('active'));
